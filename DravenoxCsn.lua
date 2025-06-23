5599cc74ecf658bdb4eb22509abf17a50781fcf6
Dravenox <satandungeon1@gmail.com>
2025-06-23 17:42:23 +0700

    Update DravenoxCsn.lua

--- DravenoxCsn.lua
+++ DravenoxCsn.lua
@@ -1,6 +1,6 @@
 for i = 1, 5 do 
   sendVariant({ [0] = 'OnTextOverlay', 
-      [1] = '`4Script Currently Maintenance, For Update!' })
+      [1] = '`5Script Currently `4M A I N T E N A N C E `5idk why?' })
   logToConsole('`2Join Discord For More Information :`5 https://dravenox-script.vercel.app (kiss)')
   sleep(1000)
 end 
