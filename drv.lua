7b254d7140541706a2a1eed1389db44becdf19dd
Dravenox <satandungeon1@gmail.com>
2025-07-21 19:22:47 +0700

    Update and rename drv.lua to DravenoxCsn-Tron.lua

--- drv.lua
+++
@@ -1,8 +0,0 @@
-for i = 1, 5 do
-logToConsole('`5Loader Script`` `^has been replaced with the new one, join Discord and take the role to gain access to the `5Loader Script``.')
-sleep(500)
-end 
-sleep(500)
-sendVariant({ [0] = 'OnTextOverlay', [1] = '`5Join Discord For Get New Loader Script !' })
-sleep(200)
-logToConsole('`5Discord Link :`` `^https://dravenox-script.vercel.app``')
